4d74b50f2e8249801f0226205967dbf7802a7a11
Patrick DeMichele <patd998@gmail.com>
2017-07-16 13:38:54 -0700

    rewrote client socket code

--- server.cc
+++ server.cc
@@ -1,17 +1,43 @@
 // server utility methods here
 #include "server.h"
+#include <netdb.h>                // for gethostbyname
+#include <sys/socket.h>           // for socket, AF_INET
+#include <sys/types.h>            // for SOCK_STREAM
+#include <unistd.h>               // for close                                 
+#include <cstring>                // for memset
+
+#include <string> // for c++ string
+
+using namespace std;
 
 // establishes an endpoint with the server.                                                     
-int fd_from_request(const std::string& request) {
-  return 0;
+//client connects to a server.
+int createClientSocket(const string& host, unsigned short port) {
+
+  struct hostent *he;
+  he = gethostbyname(host.c_str());
+  if (he == NULL) return -1;
+
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock < 0) return -1;
+
+  struct sockaddr_in serverAddress;
+  memset(&serverAddress, 0, sizeof(serverAddress));
+  serverAddress.sin_family = AF_INET;
+  serverAddress.sin_port = htons(port);
+  serverAddress.sin_addr.s_addr = ((struct in_addr *)he->h_addr)->s_addr;
+
+  if (connect(sock, (struct sockaddr *) &serverAddress, 
+	      sizeof(serverAddress)) != 0) {
+    close(sock);
+    return -1;
+  }
+  return sock;
 }
 
-int get_response(int fd, const std::string& request){
-  return 0;
-}
 
 void service_loop() {
-
+  printf("to be implemented");
 }                                                                                         
 
 
--- server.h
+++ server.h
@@ -1,9 +1,8 @@
+#define defaultPortNumber 80
+
 // Server utilies. createServerSocket, etc.
 #include <string>
+int createClientSocket(const std::string& host, unsigned short port);
 
-// establishes an endpoint with the server.
-int fd_from_request(const std::string& request);
-// gets the response
-int get_response(int fd, const std::string& request);
 
 void service_loop(); // incorporates these methods and other system calls to cycle and service clients
