ca2c69475beb508d8bc38208266f24c976022575
Patrick DeMichele <patd998@gmail.com>
2017-07-17 00:13:47 -0700

    fixed indentation / spelling

--- main.cc
+++ main.cc
@@ -11,15 +11,15 @@
 
 using namespace std;
 
-int readall(int fd, char* buf, int max_bytes, const char* sentinal) {
+int readall(int fd, char* buf, int max_bytes, const char* sentinel) {
   int num_read = 0;
-  int sentinal_size = strlen(sentinal);
+  int sentinel_size = strlen(sentinel);
   while (num_read < max_bytes) {
-	int bytes = read(fd, buf + num_read, max_bytes - num_read);
-	//if (bytes == 0) return num_read;
-	if (bytes < 0) return -num_read;
-	num_read += bytes;
-	if (num_read >= sentinal_size && strcmp(buf + num_read - sentinal_size, sentinal) == 0) return num_read;
+    int bytes = read(fd, buf + num_read, max_bytes - num_read);
+    //if (bytes == 0) return num_read;
+    if (bytes < 0) return -num_read;
+    num_read += bytes;
+    if (num_read >= sentinel_size && strcmp(buf + num_read - sentinel_size, sentinel) == 0) return num_read;
   }
   return num_read;
 }
@@ -35,28 +35,27 @@ int main(int argc, char* argv[]) {
     struct sockaddr_in clientAddr;
     socklen_t clientAddrSize = sizeof(clientAddr);
     int connfd = accept(serverfd, (struct sockaddr *)&clientAddr, &clientAddrSize);
-	if (connfd < 0) continue;
+    if (connfd < 0) continue;
 
-	char buf[BUF_SIZE];
+    char buf[BUF_SIZE];
 
-	readall(connfd, buf, BUF_SIZE, "\r\n\r\n");
-	// TODO: error checking
+    readall(connfd, buf, BUF_SIZE, "\r\n\r\n");
+    // TODO: error checking
 	
     string host = getHost(buf);
-	cout << "HOST="<<host << endl;
+    cout << "HOST="<<host << endl;
 
-	cout<<"Received request:"<<endl
-	    <<"\033[1;31m"<<buf<<"\033[0m";
-	//printf("%s", buf);
+    cout<<"Received request:"<<endl <<"\033[1;31m"<<buf<<"\033[0m";
+    //printf("%s", buf);
 	
-	int clientfd = createClientSocket(host, defaultPortNumber);
+    int clientfd = createClientSocket(host, defaultPortNumber);
     cout << "CLIENTFD = " << clientfd << endl;
-	if (clientfd > 0) close(clientfd);
+    if (clientfd > 0) close(clientfd);
 	
     write(connfd, "Niven is an idiot", 17);
     close(connfd);
 
-	cout<<endl<<endl;
+    cout<<endl<<endl;
   }
   cout << "Server is running ... just kidding" << endl;
 
